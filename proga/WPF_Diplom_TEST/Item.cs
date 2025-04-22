a45546e0e1271e5442094f863c8b1bc6dc8a0a10
KAYSER <shunkin.2004@mail.ru>
2025-04-22 19:00:06 +0300

    я того рот ебал

---
+++ proga/WPF_Diplom_TEST/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Diplom_TEST
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
