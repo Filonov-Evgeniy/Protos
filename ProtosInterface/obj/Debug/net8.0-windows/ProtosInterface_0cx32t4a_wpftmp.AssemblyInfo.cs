82b04fd3bd68577dd1c1916b4066732fe28f0172
KAYSER <shunkin.2004@mail.ru>
2025-05-11 13:22:50 +0300

    что-то обновил

--- ProtosInterface/obj/Debug/net8.0-windows/ProtosInterface_0cx32t4a_wpftmp.AssemblyInfo.cs
+++ ProtosInterface/obj/Debug/net8.0-windows/ProtosInterface_0cx32t4a_wpftmp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProtosInterface")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9885bc4f46519010d2610a8977bed9767f5cfa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProtosInterface")]
