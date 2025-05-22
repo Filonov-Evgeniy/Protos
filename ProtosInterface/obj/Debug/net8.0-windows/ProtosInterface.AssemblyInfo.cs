565be3e5c3d791d6e760a11e2cca30393d4f7723
KAYSER <shunkin.2004@mail.ru>
2025-05-22 21:59:56 +0300

    123

--- ProtosInterface/obj/Debug/net8.0-windows/ProtosInterface.AssemblyInfo.cs
+++ ProtosInterface/obj/Debug/net8.0-windows/ProtosInterface.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProtosInterface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b52d69289fa4d1bf768f58a6e787f8bc4eb08f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20aae82e43056653bb3a963ca0085b5a3955b08d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProtosInterface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProtosInterface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
